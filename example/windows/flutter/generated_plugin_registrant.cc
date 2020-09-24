e67d821c205e85aab789ba7523e8c9b663a7b682
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2020-09-24 18:04:22 +0530

    👌 Initial Windows support

---
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+#include "generated_plugin_registrant.h"
+
+#include <just_audio/just_audio_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  JustAudioPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("JustAudioPlugin"));
+}
